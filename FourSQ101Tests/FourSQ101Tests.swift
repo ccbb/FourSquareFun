6f75196c81d06e7a435a62561a2465c818c20490
Cail Borrell <cail@borrell.dk>
2016-02-02 00:09:13 +0100

    Updated app unit tests to cover configuration, request and response parsing.

--- FourSQ101Tests/FourSQ101Tests.swift
+++ FourSQ101Tests/FourSQ101Tests.swift
@@ -7,6 +7,9 @@
 //
 
 import XCTest
+import Alamofire
+import SwiftyJSON
+
 @testable import FourSQ101
 
 class FourSQ101Tests: XCTestCase {
@@ -32,5 +35,65 @@ class FourSQ101Tests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+ 
+    func testConfigURL() {
+        XCTAssertNotEqual(QueryManager.sharedInstance.url, "", "Error - url is not defined")
+    }
+
+    func testConfigClientId() {
+        XCTAssertNotEqual(QueryManager.sharedInstance.clientId, "", "Error - clientId is not defined")
+    }
+
+    func testConfigClientSecret() {
+        XCTAssertNotEqual(QueryManager.sharedInstance.clientSecret, "", "Error - clientId is not defined")
+    }
+
+    func testRequestAuthentication() {
+        let expectation = expectationWithDescription("Alamofire")
+        
+        Alamofire.request(.GET, QueryManager.sharedInstance.url!, parameters: QueryManager.generalParameters() ).response {
+            request, response, data, error in
+            
+                XCTAssertNil(error, "Whoops, error \(error)")
+                
+                XCTAssertEqual(response?.statusCode, 200, "Error - Status code not 200")
+                
+                expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
     
+    func testVenueInitializationWithValues() {
+        let name     = "VenueName"
+        let address  = "VenueAddress"
+        let city     = "VenueAddress"
+        let distance = "100"
+        
+        let jsonVenue = JSON(["name": name, "location": ["address": address, "city": city, "distance": distance]])
+        
+        let venue = Venue(json: jsonVenue)
+
+        XCTAssertEqual(venue.name, name, "Error - name is not correct")
+        XCTAssertEqual(venue.address, address, "Error - address is not correct")
+        XCTAssertEqual(venue.city, city, "Error - city is not correct")
+        XCTAssertEqual(venue.distance, Int(distance), "Error - city is not correct")
+    }
+
+    func testVenueInitializationWithoutValues() {
+        let name     = ""
+        let address  = ""
+        let city     = ""
+        let distance = ""
+        
+        let jsonVenue = JSON(["name": name, "location": ["address": address, "city": city, "distance": distance]])
+        
+        let venue = Venue(json: jsonVenue)
+        
+        XCTAssertEqual(venue.name, name, "Error - name is not correct")
+        XCTAssertEqual(venue.address, address, "Error - address is not correct")
+        XCTAssertEqual(venue.city, city, "Error - city is not correct")
+        XCTAssertEqual(venue.distance, 0, "Error - city is not correct")
+    }
+
 }
