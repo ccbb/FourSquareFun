5eaf7341f45dab08d7a1aba1e136b4bc3b281518
Cail Borrell <cail@borrell.dk>
2016-02-02 00:04:53 +0100

    Added tableview controller with search controller to display four square venues

--- FourSQ101/ViewController.swift
+++ FourSQ101/ViewController.swift
@@ -8,18 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    private let kCellIdentifier = "CellIdentifier"
+
+    // Controller used to apply search results.
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    // Array containing the fetched venues.
+    private var eventArray: [Venue]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        title = "Four Square Venues"
+        
+        definesPresentationContext = true
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
 
+        tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    // MARK: - Table View DataSource methods
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier, forIndexPath: indexPath)
+        
+        if let event = eventArray?[indexPath.row] {
+            cell.textLabel?.text = event.name
+            cell.detailTextLabel?.text = event.formattedAddress
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventArray?.count ?? 0
+    }
 
 }
 
+// MARK: - Search Result Updating Delegate methods
+
+extension ViewController: UISearchResultsUpdating {
+    
+    /**
+     Search controller updates will trigger Four Square queries.
+     */
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+        // We need to make sure we have at least one character in the query.
+        guard let query = searchController.searchBar.text where query.characters.count > 0 else { return }
+        
+        QueryManager.getVenues(query) { response in
+            switch response {
+            case .Ok(let result):
+                // The response is good and we can use the result
+                self.eventArray = result
+                self.tableView.reloadData()
+                
+            case .Error:
+                // We are in trouble so we show an alert and reset the query.
+                let alertController = UIAlertController(title: "Error", message: "Your're in trouble", preferredStyle: .Alert)
+                
+                let dismissAction = UIAlertAction(title: "Dismiss", style: .Cancel) { (action) in
+                    searchController.searchBar.text = ""
+                }
+                
+                alertController.addAction(dismissAction)
+                
+                self.presentViewController(alertController, animated: true) {}
+            }
+        }
+    }
+}
