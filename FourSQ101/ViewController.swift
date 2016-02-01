8bb4bcf31d4660acd6ac721bec99e82a3119b817
Cail Borrell <cail@borrell.dk>
2016-02-01 11:09:43 +0100

    Initial Commit

---
+++ FourSQ101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourSQ101
+//
+//  Created by Cail Borrell on 01/02/2016.
+//  Copyright © 2016 Borrell Consult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
